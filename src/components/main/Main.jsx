6ca7c7b9e80ce1d3b0d4757d0af1d3f7dc99df7a
Amanda <amandabcarneiro9@gmail.com>
2022-04-21 13:16:39 +0200

    added couple photo

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,17 @@
 import cf from "../../images/c&f.jpeg";
+import "./main.scss";
 export default function Main() {
   return (
     <div className="main">
-      <h1>Carine & Fabrício</h1>
-      <h3>" O amor se compõe de uma só Alma que habita em dois corpos"</h3>
-      <span>17 setembro, 2022 |Caçapava - SP</span>
-      <img src={cf} alt="Fabricio e Carine" />
+      <h1 className="__names">Carine & Fabrício</h1>
+      <h3 className="__mention">
+        " O amor se compõe de uma só alma que habita em dois corpos"
+      </h3>
+
+      <div className="__photo">
+        <img src={cf} alt="Fabricio e Carine" />
+      </div>
+      <span className="__date">17 setembro, 2022 | Caçapava - SP</span>
     </div>
   );
 }
