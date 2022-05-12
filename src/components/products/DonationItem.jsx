ce9824c07b4e464dec01b97afc34edba00de1139
Amanda <amandabcarneiro9@gmail.com>
2022-05-12 16:49:43 +0200

    smaller font size for desktop on gallery

--- src/components/products/DonationItem.jsx
+++ src/components/products/DonationItem.jsx
@@ -4,7 +4,7 @@ export default function ItemDonation({ name, price }) {
   return (
     <div className="produto">
       <h3 className="__item">{name}</h3>
-      <span className="__price">`R${price}`</span>
+      <span className="__price">R${price}</span>
       <button>Ajudar</button>
     </div>
   );
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,8 +4,8 @@ import "./products.scss";
 export default function ProductsSession() {
   return (
     <div className="product_container">
-      <h2>
-        Você tambêm pode nos ajudar com qualquer quantia para nossa Lua de Mel
+      <h2 className="__content">
+        Você também pode nos ajudar com qualquer quantia para nossa Lua de Mel
         escolhendo algum dos itens abaixou ou doando uma quantia direta
       </h2>
       <div className="__products">
@@ -15,9 +15,7 @@ export default function ProductsSession() {
         <DonationItem name="Drinks para o casal" price="50,00" />
         <DonationItem name="Drinks para o casal" price="50,00" />
         <DonationItem name="Drinks para o casal" price="50,00" />
-
         <DonationItem name="Drinks para o casal" price="50,00" />
-
         <DonationItem name="Drinks para o casal" price="50,00" />
       </div>
     </div>
